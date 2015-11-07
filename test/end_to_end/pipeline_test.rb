40da9440d3ab962f9c052466f97fd6a3e316a12a
Matijs van Zuijlen <matijs@matijs.net>
2015-11-07 11:33:58 +0100

    Remove redundant Pipeline test

--- test/end_to_end/pipeline_test.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../test_helper'
-
-describe Gst::Pipeline do
-  it 'allows easy enumeration of its elements' do
-    pipeline = Gst::ElementFactory.make 'playbin', nil
-    elements = pipeline.iterate_elements
-    elements.map(&:get_name).must_equal ['playsink']
-  end
-end
