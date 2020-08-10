fdbd49a995a17e1acfb6be55432d6afe24f9299e
Matijs van Zuijlen <matijs@matijs.net>
2020-08-10 09:08:27 +0200

    Add rubocop-packaging cops and fix offenses

--- gir_ffi-gst.gemspec
+++ gir_ffi-gst.gemspec
@@ -27,5 +27,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 13.0"
   spec.add_development_dependency "rubocop", "~> 0.89.0"
   spec.add_development_dependency "rubocop-minitest", "~> 0.10.1"
+  spec.add_development_dependency "rubocop-packaging", "~> 0.2.0"
   spec.add_development_dependency "rubocop-performance", "~> 1.7.0"
 end
--- lib/gir_ffi-gst.rb
+++ lib/gir_ffi-gst.rb
@@ -5,8 +5,8 @@ require "gir_ffi"
 GirFFI.setup :Gst
 GirFFI.setup :GstBase
 
-require_relative "gir_ffi-gst/base"
-require_relative "gir_ffi-gst/bin"
-require_relative "gir_ffi-gst/child_proxy"
-require_relative "gir_ffi-gst/element"
-require_relative "gir_ffi-gst/iterator"
+require "gir_ffi-gst/base"
+require "gir_ffi-gst/bin"
+require "gir_ffi-gst/child_proxy"
+require "gir_ffi-gst/element"
+require "gir_ffi-gst/iterator"
